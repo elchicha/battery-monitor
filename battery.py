698d76bbe1edbb9a08195ccd4fb183db3ed9dfc1
elchicha <github.com.bulginess438@passmail.com>
2026-01-07 19:23:40 -0800

    feat: implement get_battery_percentage get_charging_status

--- battery.py
+++ battery.py
@@ -1,4 +1,23 @@
-
-
 class BatteryChecker:
-    pass
+    def __init__(self):
+        self.level = 0
+
+    def get_percentage(self):
+        """Retrieve the battery percentage level.
+
+        Returns:
+            int or float: Battery percentage level.
+        """
+        with open("/sys/class/power_supply/BAT0/capacity") as capacity_file:
+            self.level = int(capacity_file.read().strip())
+        return self.level
+
+    def is_charging(self):
+        """Check if the battery is currently charging.
+
+        Returns:
+            bool: True if charging, False otherwise.
+        """
+        with open("/sys/class/power_supply/BAT0/status") as status_file:
+            status = status_file.read().strip()
+        return status == "Charging"
