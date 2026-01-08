0f951cc266ab44db92480032cd0d27bcaddfc71d
elchicha <github.com.bulginess438@passmail.com>
2026-01-07 19:42:06 -0800

    test: get time remaining based on power calculation

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -17,3 +17,13 @@ def test_get_charging_status():
     checker = BatteryChecker()
     is_charging = checker.is_charging()
     assert isinstance(is_charging, bool)
+
+
+def test_get_time_remaining():
+    checker = BatteryChecker()
+    time_remaining = checker.get_time_remaining()
+    # Time remaining could be None (if can't calculate) or a positive number
+    print(f"Time left: {time_remaining} minutes")
+    assert time_remaining is None or (
+        isinstance(time_remaining, (int, float)) and time_remaining >= 0
+    )
