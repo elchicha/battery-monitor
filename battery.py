55561f786a8a2d8213975f71dbc6367e75ba849b
elchicha <github.com.bulginess438@passmail.com>
2026-01-07 19:42:31 -0800

    feat: implement time remaing (charging and discharging)

--- battery.py
+++ battery.py
@@ -21,3 +21,23 @@ class BatteryChecker:
         with open("/sys/class/power_supply/BAT0/status") as status_file:
             status = status_file.read().strip()
         return status == "Charging"
+
+    def get_time_remaining(self):
+        """Estimate the remaining time until battery depletion.
+
+        Returns:
+            int or float or None: Estimated time remaining in minutes, or None if cannot be calculated.
+        """
+        charge_now = int(open("/sys/class/power_supply/BAT0/energy_now").read().strip())
+        charge_full = int(
+            open("/sys/class/power_supply/BAT0/energy_full").read().strip()
+        )
+        power_now = int(open("/sys/class/power_supply/BAT0/power_now").read().strip())
+
+        if charge_now == 0 or charge_full == 0 or power_now == 0:
+            return None
+        if self.is_charging():
+            time_remaining = (charge_full - charge_now) / power_now
+        else:
+            time_remaining = charge_now / power_now
+        return time_remaining * 60
