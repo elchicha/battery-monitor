ef4c5dccfd1f023838ab1a5e2ae7a506492ad3bb
elchicha <github.com.bulginess438@passmail.com>
2026-01-07 19:22:58 -0800

    test: test battery percentage, charging status

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -1,5 +1,19 @@
 from battery import BatteryChecker
 
+
 def test_can_create_battery_checker():
     checker = BatteryChecker()
     assert checker is not None
+
+
+def test_get_battery_percentage():
+    checker = BatteryChecker()
+    level = checker.get_percentage()
+    assert isinstance(level, (int, float))
+    assert level >= 0 and level <= 100
+
+
+def test_get_charging_status():
+    checker = BatteryChecker()
+    is_charging = checker.is_charging()
+    assert isinstance(is_charging, bool)
